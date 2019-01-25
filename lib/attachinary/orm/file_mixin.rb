aadaaee64dc7edfd8eff99540a93a68c34a2c0d8
Anton <ipatovanton@gmail.com>
2019-01-25 10:41:23 +0300

    Update file_mixin.rb

--- lib/attachinary/orm/file_mixin.rb
+++ lib/attachinary/orm/file_mixin.rb
@@ -16,7 +16,7 @@ module Attachinary
     end
 
     def as_json(options)
-      super(only: [:id, :public_id, :format, :version, :resource_type], methods: [:path])
+      super(only: [:id, :public_id, :format, :version, :resource_type, :position], methods: [:path])
     end
 
     def path(custom_format=nil)
