8ff9ec4e88eaa01be2201ea286ed78da35206d43
Anton <ipatovanton@gmail.com>
2019-12-05 13:02:54 +0300

    Update file.rb

--- lib/attachinary/orm/mongoid/file.rb
+++ lib/attachinary/orm/mongoid/file.rb
@@ -2,6 +2,7 @@ module Attachinary
   class File
     include ::Mongoid::Document
     include ::Mongoid::Timestamps
+    include Mongoid::Attributes::Dynamic
     include FileMixin
 
     field :public_id, type: String
@@ -10,6 +11,7 @@ module Attachinary
     field :height, type: Integer
     field :format, type: String
     field :resource_type, type: String
+    field :position, type: Integer, default: 0
 
     embedded_in :attachinariable, polymorphic: true
   end
